df6730d83e0f192af3febf8aaf8f58e3a9145cb0
vg <vg@vgmb.lan>
2018-07-25 12:49:42 -0500

    Added some more tests.

--- RESTAPITestingWithJava/src/TestSuite/Tests.java
+++ RESTAPITestingWithJava/src/TestSuite/Tests.java
@@ -1,15 +1,11 @@
 package TestSuite;
 
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,14 +14,17 @@ import java.util.TimeZone;
 
 public class Tests
 {  
+	//Variables
 	private static int successStatusCode = 200;
 	private static String responseContentType = "JSON";
 	private static String repoName = "MyFirstRepo";
 	private static String repoOwnerName = "venug0453";
+	private static String baseURI = "https://api.github.com/repos/" + repoOwnerName + "/" + repoName;	
 	
+	//Get Response
 	public Response GetResponse()
 	{
-		RestAssured.baseURI = "https://api.github.com/repos/" + repoOwnerName + "/" + repoName;		
+		RestAssured.baseURI = baseURI;		
 		Response response = when().get().then().extract().response();		
 		return response;
 	}
@@ -59,7 +58,7 @@ public class Tests
 	}
 	
 	@Test
-	public void ValidateResponseTimeStampFormat()
+	public void ValidateResponseDateHeaderFormat()
 	{
 		String responseTimeStamp = GetResponse().getHeader("Date");
 		Date date = new Date();
@@ -67,19 +66,38 @@ public class Tests
 		df.setTimeZone(TimeZone.getTimeZone("GMT"));
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
-		calendar.add(Calendar.MINUTE, 1);
+		calendar.add(Calendar.SECOND, 1);
 		String dateStr = df.format(date);
 		String dateStr1 = df.format(calendar.getTime());
 		
-		if(responseTimeStamp.equals(dateStr))
-		{
-		}	
-		else if(responseTimeStamp.equals(dateStr1))
-		{			
-		}
-		else
-		{
-			fail("Time stamp under the 'Date' field of the response header is not as expected.");
-		}
+		assertTrue((responseTimeStamp.equals(dateStr) || responseTimeStamp.equals(dateStr1)), "The 'Date' field under the response header is not as expected.");		
+	}
+	
+	@Test
+	public void ValidateTimeStampsFormatUnderResponse()
+	{
+		Response response = GetResponse();
+		String responseCreatedAtTimeStamp = response.jsonPath().getString("created_at").toString();
+		String responseUpdatedAtTimeStamp = response.jsonPath().getString("updated_at").toString();
+		String responsePushedAtTimeStamp = response.jsonPath().getString("pushed_at").toString();
+		
+		String matchPattern = "[\\d]{4}\\-[\\d]{2}\\-[\\d]{2}T[\\d]{2}:[\\d]{2}:[\\d]{2}Z";
+		assertTrue(responseCreatedAtTimeStamp.matches(matchPattern), "'created_at' field under the response is not in the expected format.");
+		assertTrue(responseUpdatedAtTimeStamp.matches(matchPattern), "'updated_at' field under the response is not in the expected format.");
+		assertTrue(responsePushedAtTimeStamp.matches(matchPattern), "'pushed_at' field under the response is not in the expected format.");
+	}
+	
+	@Test
+	public void ValidateResponseURLField()
+	{
+		String urlFieldResponse = GetResponse().jsonPath().getString("url").toString();
+		assertEquals(urlFieldResponse, baseURI, "Expected 'url' field <" + baseURI + "> but was <" + urlFieldResponse + ">.");
+	}
+	
+	@Test
+	public void ValidateOwnerLoginFromResponse()
+	{
+		String ownerLogin = GetResponse().jsonPath().getString("owner.login").toString();
+		assertEquals(ownerLogin, repoOwnerName, "Expected 'owner.login' field <" + repoOwnerName + "> but was <" + ownerLogin + ">.");
 	}
 }
